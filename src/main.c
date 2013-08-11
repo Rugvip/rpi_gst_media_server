09a23052f36e60efc74571a8f21b9b1e9861fd94
Rugvip <poldsberg@gmail.com>
2013-08-11 19:50:12 +0200

    Removed useless server status spam

--- src/main.c
+++ src/main.c
@@ -7,32 +7,6 @@
 
 #define PORT 3264
 
-void server_send_playback_status(Server *server)
-{
-    ResponsePlaying response = {
-        .song = server->player->source[0]->song,
-        .duration = player_get_duration(server->player),
-        .position = player_get_position(server->player),
-    };
-
-    jsonio_broadcast_packet(server, jsonio_response_playing_packet(&response));
-}
-
-gboolean server_print_connections(Server *server)
-{
-    gint i;
-    GPtrArray *clients = server->clients;
-    gint len = server->clients->len;
-
-    g_print("%d clients connected\n", len);
-    for (i = 0; i < len; ++i) {
-        Client *client;
-        client = g_ptr_array_index(clients, i);
-        g_print("%s:%d\n", client->remote_address, client->remote_port);
-    }
-    return TRUE;
-}
-
 int main(int argc, const char *argv[])
 {
     g_print("Starting %s\n", argv[0]);
@@ -47,9 +21,6 @@ int main(int argc, const char *argv[])
 
     server_init(server);
     player_init(server->player);
-
-    g_timeout_add(60000, (GSourceFunc) server_print_connections, server);
-    g_timeout_add(10000, (GSourceFunc) server_send_playback_status, server);
 
     server_start(server, PORT);
     player_start(server->player);
