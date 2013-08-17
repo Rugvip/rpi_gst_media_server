0930af6b27ea2f8c3d4f734f4b6ef139f00b5f2a
Rugvip <poldsberg@gmail.com>
2013-08-17 22:04:15 +0200

    stdio: updated jsongen

--- src/jsongen.c
+++ src/jsongen.c
@@ -5,62 +5,32 @@
 #include <glib.h>
 #include <json-glib/json-glib.h>
 
-#define JSON_PACKET_PRIVATE(obj) ((JsonPacketPrivate *) ((obj)->priv))
-#define JSON_PACKET_BUILDER(obj) (JSON_PACKET_PRIVATE(obj)->builder)
+#define add_string(builder, name, value) json_builder_add_string_value \
+    (json_builder_set_member_name(builder, name), value)
+#define add_int(builder, name, value) json_builder_add_int_value \
+    (json_builder_set_member_name(builder, name), value)
+#define add_double(builder, name, value) json_builder_add_double_value \
+    (json_builder_set_member_name(builder, name), value)
 
-#define add_string(builder, name, value) json_builder_add_string_value( \
-    json_builder_set_member_name(builder, name), value)
-#define add_int(builder, name, value) json_builder_add_int_value( \
-    json_builder_set_member_name(builder, name), value)
-#define add_double(builder, name, value) json_builder_add_double_value( \
-    json_builder_set_member_name(builder, name), value)
-
-typedef struct {
-    JsonBuilder *builder;
-} JsonPacketPrivate;
-
-void jsongen_write_packet(GOutputStream *out, JsonPacket *packet)
+static void write_packet(Output *output, JsonBuilder *builder)
 {
-    JsonNode *root;
     GError *error = NULL;
     JsonGenerator *generator;
 
-    root = json_builder_get_root(JSON_PACKET_PRIVATE(packet)->builder);
-
     generator = json_generator_new();
-    json_generator_set_root(generator, root);
-    json_generator_to_stream(generator, out, NULL, &error);
+    json_generator_set_root(generator, json_builder_get_root(builder));
+    json_generator_to_stream(generator, output->player->out, NULL, &error);
 
     if (error) {
         g_warning("Error writing json output stream: %s\n", error->message);
+        g_clear_error(&error);
     }
-    g_clear_error(&error);
+
     g_object_unref(G_OBJECT(generator));
+    g_object_unref(G_OBJECT(builder));
 }
 
-void jsongen_free_packet(JsonPacket *packet)
-{
-    JsonPacketPrivate *priv = JSON_PACKET_PRIVATE(packet);
-
-    g_object_unref(G_OBJECT(priv->builder));
-
-    g_free(priv);
-    g_free(packet);
-}
-
-static JsonPacket *json_packet_new()
-{
-    JsonPacket *packet;
-
-    packet = g_new0(JsonPacket, 1);
-    packet->priv = g_new0(JsonPacketPrivate, 1);
-
-    JSON_PACKET_PRIVATE(packet)->builder = json_builder_new();
-
-    return packet;
-}
-
-JsonPacket *jsongen_playing(Song song, gint duration, gint position)
+void jsongen_playing(OutputPlaying *output)
 {
     JsonPacket *packet = json_packet_new();
     JsonBuilder *builder = JSON_PACKET_BUILDER(packet);
@@ -68,18 +38,18 @@ JsonPacket *jsongen_playing(Song song, gint duration, gint position)
     json_builder_begin_object(builder);
 
     add_string(builder, "type", "played");
-    add_string(builder, "artist", song.artist);
-    add_string(builder, "album", song.album);
-    add_string(builder, "song", song.name);
-    add_int(builder, "duration", duration);
-    add_int(builder, "position", position);
+    add_string(builder, "artist", output->song.artist);
+    add_string(builder, "album", output->song.album);
+    add_string(builder, "song", output->song.name);
+    add_int(builder, "duration", output->duration);
+    add_int(builder, "position", output->position);
 
     json_builder_end_object(builder);
 
-    return packet;
+    write_packet(&output->output, builder);
 }
 
-JsonPacket *jsongen_paused(gint position)
+void jsongen_paused(OutputPaused *output)
 {
     JsonPacket *packet = json_packet_new();
     JsonBuilder *builder = JSON_PACKET_BUILDER(packet);
@@ -87,14 +57,14 @@ JsonPacket *jsongen_paused(gint position)
     json_builder_begin_object(builder);
 
     add_string(builder, "type", "paused");
-    add_int(builder, "position", position);
+    write_packet(&output->output, builder);
 
     json_builder_end_object(builder);
 
-    return packet;
+    write_packet(&output->output, builder);
 }
 
-JsonPacket *jsongen_eq(gdouble bands[NUM_EQ_BANDS])
+void jsongen_eq(OutputEq *output)
 {
     int i;
     JsonPacket *packet = json_packet_new();
@@ -106,16 +76,16 @@ JsonPacket *jsongen_eq(gdouble bands[NUM_EQ_BANDS])
 
     json_builder_begin_array(json_builder_set_member_name(builder, "bands"));
     for (i = 0; i < NUM_EQ_BANDS; ++i) {
-        json_builder_add_double_value(builder, bands[i]);
+        json_builder_add_double_value(builder, output->bands[i]);
     }
     json_builder_end_array(builder);
 
     json_builder_end_object(builder);
 
-    return packet;
+    write_packet(&output->output, builder);
 }
 
-JsonPacket *jsongen_volume(gdouble volume)
+void jsongen_volume(OutputVolume *output)
 {
     JsonPacket *packet = json_packet_new();
     JsonBuilder *builder = JSON_PACKET_BUILDER(packet);
@@ -123,14 +93,14 @@ JsonPacket *jsongen_volume(gdouble volume)
     json_builder_begin_object(builder);
 
     add_string(builder, "type", "volume");
-    add_double(builder, "volume", volume);
+    add_double(builder, "volume", output->volume);
 
     json_builder_end_object(builder);
 
-    return packet;
+    write_packet(&output->output, builder);
 }
 
-JsonPacket *jsongen_info(Song song, gint duration, gint position)
+void jsongen_info(OutputInfo *output)
 {
     JsonPacket *packet = json_packet_new();
     JsonBuilder *builder = JSON_PACKET_BUILDER(packet);
@@ -138,13 +108,13 @@ JsonPacket *jsongen_info(Song song, gint duration, gint position)
     json_builder_begin_object(builder);
 
     add_string(builder, "type", "info");
-    add_string(builder, "artist", song.artist);
-    add_string(builder, "album", song.album);
-    add_string(builder, "song", song.name);
-    add_int(builder, "duration", duration);
-    add_int(builder, "position", position);
+    add_string(builder, "artist", output->song.artist);
+    add_string(builder, "album", output->song.album);
+    add_string(builder, "song", output->song.name);
+    add_int(builder, "duration", output->duration);
+    add_int(builder, "position", output->position);
 
     json_builder_end_object(builder);
 
-    return packet;
+    write_packet(&output->output, builder);
 }
