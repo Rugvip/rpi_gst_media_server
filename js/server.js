9530f8312a99f49ec6b5b80b3c09f46d6ded2ac5
Rugvip <poldsberg@gmail.com>
2013-08-19 13:30:50 +0200

    Duration query tests

--- js/server.js
+++ js/server.js
@@ -1,3 +1,4 @@
+
 "use strict";
 
 var http     = require('http'),
@@ -11,7 +12,7 @@ var http     = require('http'),
     spawn    = require('child_process').spawn;
 
 module.exports.start = function () {
-    var gst, obj;
+    var gst, objs;
 
     console.log("Starting media gst");
 
@@ -28,12 +29,69 @@ module.exports.start = function () {
         console.log("Err: ", data);
     });
 
-
-    obj = {
-        type: "volume",
-        volume: 1.5
-    };
-    gst.stdin.write(JSON.stringify(obj));
+    objs = [
+        {
+            type: "duration_query",
+            artist: "Youtube Mixes",
+            album: "Hardcore",
+            song: "DJ Gonzo I",
+        },
+        {
+            type: "duration_query",
+            artist: "Youtube Mixes",
+            album: "Hardcore",
+            song: "DJ Gonzo II",
+        },
+        {
+            type: "duration_query",
+            artist: "Youtube Mixes",
+            album: "Industrial",
+            song: "Snow Styler VI",
+        },
+        {
+            type: "duration_query",
+            artist: "Youtube Mixes",
+            album: "Industrial",
+            song: "Dark Modulator I",
+        },
+        {
+            type: "duration_query",
+            artist: "Youtube Mixes",
+            album: "Industrial",
+            song: "Dark Modulator II",
+        },
+        {
+            type: "duration_query",
+            artist: "Youtube Mixes",
+            album: "Industrial",
+            song: "Dark Modulator III",
+        },
+        {
+            type: "duration_query",
+            artist: "Youtube Mixes",
+            album: "Industrial",
+            song: "Dark Modulator IV",
+        },
+        {
+            type: "duration_query",
+            artist: "Youtube Mixes",
+            album: "Industrial",
+            song: "Dark Modulator V",
+        },
+        {
+            type: "duration_query",
+            artist: "Youtube Mixes",
+            album: "Industrial",
+            song: "Dark Modulator VI",
+        },
+    ];
+    setTimeout(function () {
+        objs.forEach(function (obj) {
+            console.log("WRITIN");
+            gst.stdin.write(JSON.stringify(obj));
+            gst.stdin.end();
+        });
+    }, 2000);
 };
 
 
