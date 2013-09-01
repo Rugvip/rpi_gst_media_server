b705c203dbaa9f3e0a0725fc1b0d6fead7d2355a
Rugvip <poldsberg@gmail.com>
2013-09-01 13:25:12 +0200

    added transition utils

--- module/src/gst_utils.c
+++ module/src/gst_utils.c
@@ -180,3 +180,83 @@ void song_query_duration(Song song, SongDurationQueryCallback callback, gpointer
 
     element_query_duration_async(query->pipeline, (ElementDurationQueryCallback)song_query_duration_callback, query);
 }
+
+typedef struct {
+    gpointer user_data;
+    guint duration;
+    gint64 start_time;
+    TransitionTickCallback tick;
+    TransitionDoneCallback done;
+} TransitionData;
+
+static gboolean transition_timeout(TransitionData *data)
+{
+    gint64 running_time = g_get_monotonic_time() - data->start_time;
+    if (running_time < data->duration) {
+        gboolean ret;
+        gfloat progress = running_time / (gfloat) data->duration;
+
+        if (progress < 0.0f) {
+            progress = 0.0f;
+        } else if (progress > 1.0f) {
+            progress = 1.0f;
+        }
+
+        ret = data->tick(progress, data->user_data);
+
+        if (ret) {
+            return TRUE;
+        } else {
+            g_free(data);
+            return FALSE;
+        }
+    } else {
+        data->done(data->user_data);
+
+        g_free(data);
+        return FALSE;
+    }
+}
+
+void do_transition(guint duration, TransitionTickCallback tick,
+    TransitionDoneCallback done, gpointer user_data)
+{
+    TransitionData *data;
+
+    data = g_new0(TransitionData, 1);
+    data->user_data = user_data;
+    data->duration = duration * 1000;
+    data->start_time = g_get_monotonic_time();
+    data->tick = tick;
+    data->done = done;
+
+    g_timeout_add(20, (GSourceFunc) transition_timeout, data);
+}
+
+typedef struct {
+    GstBin *bin;
+    GstPipeline *pipeline;
+} RemoveBinFromPipelineData;
+
+GstPadProbeReturn remove_bin_from_pipeline_callback(GstPad *pad,
+    GstPadProbeInfo *info, RemoveBinFromPipelineData *data)
+{
+    gst_pad_remove_probe(pad, GST_PAD_PROBE_INFO_ID (info));
+
+    gst_bin_remove(GST_BIN(data->pipeline), GST_ELEMENT(data->bin));
+    gst_element_set_state(GST_ELEMENT(data->bin), GST_STATE_READY);
+
+    return GST_PAD_PROBE_OK;
+}
+
+void remove_bin_from_pipeline(GstBin *bin, GstPipeline *pipeline, GstPad *pad)
+{
+    RemoveBinFromPipelineData *data;
+
+    data = g_new0(RemoveBinFromPipelineData, 1);
+    data->bin = bin;
+    data->pipeline = pipeline;
+
+    gst_pad_add_probe(pad, GST_PAD_PROBE_TYPE_BLOCK_DOWNSTREAM,
+        (GstPadProbeCallback) remove_bin_from_pipeline_callback, data, NULL);
+}
--- module/src/gst_utils.h
+++ module/src/gst_utils.h
@@ -7,6 +7,8 @@ G_BEGIN_DECLS
 
 typedef void (*SongDurationQueryCallback)(Song, gint64 duration, gpointer user_data);
 typedef void (*ElementDurationQueryCallback)(gint64 duration, gpointer user_data);
+typedef void (*TransitionDoneCallback)(gpointer user_data);
+typedef gboolean (*TransitionTickCallback)(gfloat progress, gpointer user_data);
 
 gint64 element_query_duration(GstElement *);
 gint64 element_query_position(GstElement *);
@@ -14,6 +16,10 @@ gboolean source_set_song_async(GstElement *, Song);
 gboolean source_set_song_sync(GstElement *, Song);
 void element_query_duration_async(GstElement *, ElementDurationQueryCallback, gpointer user_data);
 void song_query_duration(Song, SongDurationQueryCallback, gpointer user_data);
+
+void do_transition(guint duration, TransitionTickCallback, TransitionDoneCallback, gpointer user_data);
+
+void remove_bin_from_pipeline(GstBin *, GstPipeline *, GstPad *);
 
 G_END_DECLS
 
